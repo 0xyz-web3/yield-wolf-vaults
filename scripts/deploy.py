1e7b4067822089f4977e9d91eeeabe59c4e76ed3
0xyz-web3 <0xyz.web3@gmail.com>
2022-05-10 14:21:14 +1000

    Modified deploy.py to remove unneccessary code.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,20 +3,19 @@ from brownie import ACSolidex, YieldWolf, interface
 from web3 import Web3
 from scripts.token_scripts import get_lp_token, get_wrapped_ftm
 
-AMOUNT_OF_FTM_TO_EXCHANGE = 20 * 10 ** 18
-FTM_TO_BE_WRAPPED = 10 * 10 ** 18
-
 TOKEN0_NAME = 'wftm'
 TOKEN1_NAME = 'tomb'
-LIQUIDITY_PAIR_NAME = 'wftm_tomb_spookyswap_pair'
-ROUTER_CONTRACT_ADDRESSES = {'uniswap_router': {'ftm-mainnet-fork': ['0xF491e7B69E4244ad4002BC14e878a34207E38c29']}, 
-                             'solidly_router': {'ftm-mainnet-fork': ['0xa38cd27185a464914D3046f0AB9d43356B34829D']}}
+ROUTER = 'solidly'
+
+# This script deploys a YieldWolf vault, deploys a strategy and initiialises the strategy for the solidex farm which 
+# autocompounds rewards for the wftm-tomb solidly volatile LP farm. Finally the wftm-tomb solidly volatile LP token 
+# is deposited in the vault pool which is governed by the deployed strategy.
+
 def deploy_vault():
     account = get_account()
     fee_address = account
     vault = YieldWolf.deploy(fee_address, {"from": account})
     return vault
-    
 
 def deploy_strategy():
     account = get_account()
@@ -28,12 +27,12 @@ def deploy_strategy():
     POOL_ID = 0
     IS_LP = True
     YIELD_WOLF_CONTRACT = vault.address
-    EARN_TOKEN = "0xd31fcd1f7ba190dbc75354046f6024a9b86014d7"
-    STAKE_TOKEN = "0x60a861cd30778678e3d613db96139440bd333143"
-    MASTERCHEF = "0x26e1a0d851cf28e697870e1b7f053b605c8b060f"
-    SWAP_ROUTER = "0xf491e7b69e4244ad4002bc14e878a34207e38c29"
-    LIQUIDITY_ROUTER = "0xa38cd27185a464914d3046f0ab9d43356b34829d"
-    WNATIVE = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83"
+    EARN_TOKEN = "0xd31fcd1f7ba190dbc75354046f6024a9b86014d7" # solidex
+    STAKE_TOKEN = "0x60a861cd30778678e3d613db96139440bd333143" # wftm-tomb solidly volitile LP
+    MASTERCHEF = "0x26e1a0d851cf28e697870e1b7f053b605c8b060f" # solidex masterchef
+    SWAP_ROUTER = "0xf491e7b69e4244ad4002bc14e878a34207e38c29" # spookyswap router
+    LIQUIDITY_ROUTER = "0xa38cd27185a464914d3046f0ab9d43356b34829d" # solidly router
+    WNATIVE = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83" # wftm token
     ADDRESSES = [YIELD_WOLF_CONTRACT, STAKE_TOKEN, EARN_TOKEN, MASTERCHEF, SWAP_ROUTER, LIQUIDITY_ROUTER, WNATIVE]
     
     EARN_TO_TOKEN0_PATH = ["0x6c021Ae822BEa943b2E66552bDe1D2696a53fbB7"]
@@ -52,10 +51,6 @@ def add_strategy_to_vault(vault, strategy):
     vault.add(strategy, {"from": account})
     print("Strategy added to vault.")
 
-def get_wrapped_ftm_deploy(FTM_TO_BE_WRAPPED):
-    tx, wrapped_ftm_contract = get_wrapped_ftm(FTM_TO_BE_WRAPPED)
-    return tx, wrapped_ftm_contract
-    
 def deposit_stake_token_in_vault(vault, strategy, ftm_to_be_wrapped, token0_name, token1_name, router_type, route_is_stable):
     account = get_account()
     router = get_lp_token(ftm_to_be_wrapped, token0_name, token1_name, router_type, route_is_stable)
@@ -69,10 +64,9 @@ def deposit_stake_token_in_vault(vault, strategy, ftm_to_be_wrapped, token0_name
     
     return tx
 
-
 def main():
     vault = deploy_vault()
     strategy = deploy_strategy()
     add_strategy_to_vault(vault, strategy)
-    deposit_stake_token_in_vault(vault, strategy, 50*10**18, TOKEN0_NAME, TOKEN1_NAME, 'solidly', False)
+    deposit_stake_token_in_vault(vault, strategy, 50*10**18, TOKEN0_NAME, TOKEN1_NAME, ROUTER, False)
     
